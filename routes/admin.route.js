d263cca65af25100fbc655400816d33e8c83fe6a
ArturoReyes1512 <167336219+ArturoReyes1512@users.noreply.github.com>
2024-05-24 00:31:12 -0600

    Modificar el método POST

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,3 +6,14 @@ const products = [];
 
 // Exportando los productos
 module.exports = { router, products };
+
+// POST /admin/add-product
+router.post('/add-product', (req, res) => {
+  // Realizaremos la desestructuración de "title" de la petición
+  const { title } = req.body;
+  // Agregamos el dato en la base de datos en memoria
+  products.push(title);
+  // Redireccionando a la página principal
+  res.redirect('/');
+});
+
